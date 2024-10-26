29d7d1e1fbad52ad6dda83eaae6b9879648e3f17
Md Ariful Islam <arifulatwork@gmail.com>
2024-10-26 11:31:14 +0300

    Working

--- script.js
+++ script.js
@@ -1,116 +1,102 @@
-// Variables for Movement
-
-var PressLeft = 0;
-var PressRight = 0;
-var PressForward = 0;
-var PressBack =0;
-var pressUp = 0;
-var MouseX = 0;
-var MouseY = 0;
-
-// add the player 
-
-function player(x,y,z,rx,ry){
-
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.rx = rx;
-    this.ry = ry;
-    // this.player = document.createElement('div');
-
+// Player constructor
+class Player {
+    constructor(x, y, z, rx, ry) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.rx = rx;
+        this.ry = ry;
+    }
 }
 
+// Variables for movement
+let PressLeft = 0;
+let PressRight = 0;
+let PressForward = 0;
+let PressBack = 0;
+let PressUp = 0;
+let MouseX = 0;
+let MouseY = 0;
 
-//if the key is pressed events, set the corresponding variable to 1. This will enable the movement of the camera.
-
-document.addEventListener('keydown' ,(event) => {
-    if(event.key === 'a') {
-    PressLeft = 1;
+// Key press events
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "a":
+            PressLeft = 1;
+            break;
+        case "d":
+            PressRight = 1;
+            break;
+        case "w":
+            PressForward = 1;
+            break;
+        case "s":
+            PressBack = 1;
+            break;
+        case " ":
+            PressUp = 1; // Space bar
+            break;
     }
-        
-    
-    if(event.key === 'd') {
-        PressRight = 1;
+});
+
+// Key release events
+document.addEventListener("keyup", (event) => {
+    switch (event.key) {
+        case "a":
+            PressLeft = 0;
+            break;
+        case "d":
+            PressRight = 0;
+            break;
+        case "w":
+            PressForward = 0;
+            break;
+        case "s":
+            PressBack = 0;
+            break;
+        case " ":
+            PressUp = 0; // Space bar
+            break;
     }
+});
 
-    if(event.key === 'w') {
-        PressForward = 1;
-    }
+// Mouse movement event
+document.addEventListener("mousemove", (event) => {
+    MouseX += event.movementX;
+    MouseY += event.movementY;
+});
 
-    if(event.key ==='s') {
-        PressBack = 1;
-    }
+// Create a player instance
+const pawn = new Player(0, 0, 0, 0, 0);
+const world = document.getElementById("world");
 
-    if(event.keyCode === 32) {
-        pressUp = 1;
-    }
+function update() {
+    // Count movement
+    const dx = PressRight - PressLeft;
+    const dz = -(PressForward - PressBack);
+    const dy = PressUp;
+    const drx = MouseY;
+    const dry = -MouseX;
 
+    // Reset mouse movement
+    MouseX = 0;
+    MouseY = 0;
 
-})
+    // Update pawn coordinates
+    pawn.x += dx;
+    pawn.y += dy;
+    pawn.z += dz;
+    pawn.rx += drx;
+    pawn.ry += dry;
 
-// if the key is releaseEvents, set the corresponding variable to 0.
-
-document.addEventListener('keyup' ,(event) => {
-    if(event.key === 'a') {
-    PressLeft = 0;
-    }
-        
-    
-    if(event.key === 'd') {
-        PressRight = 0;
-    }
-
-    if(event.key === 'w') {
-        PressForward = 0;
-    }
-
-    if(event.key ==='s') {
-        PressBack = 0;
-    }
-
-    if(event.keyCode === 38) {
-        pressUp = 0;
-    }
-
-
-})
-
-document.addEventListener('mousemove', (event)  =>{
-    MouseX = event.movementX;
-    MouseY = event.movementY;
-})
-
-
-
-var pawn = new player(0,0,0,0,0);
-
-
-var world = document.getElementById('world');
-
-
-function update(){
-    // count movement
-    dx = PressRight - PressLeft;
-    dz = PressForward - PressBack;
-    dy = pressUp;
-    drx = MouseY;
-    dry = MouseX;
-    
-
-    
-
-    // add Movemnent to the coordinate
-    pawn.x = pawn.x + dx;
-    pawn.z = pawn.z + dz;
-    pawn.y = pawn.y + dy;
-    pawn.rx = pawn.rx + drx;
-    pawn.ry = pawn.ry + dry;
-
-
-    // move the world based on the new coordinates
-    world.style.transform = "rotateX("+(-pawn.rx)+"deg)+ rotateY("+(-pawn.ry)+"deg)"+
-    "translate3d("+(-pawn.x) +"px," +(-pawn.y) +"px,"+ (-pawn.z) +"px)";
+    // Move the world based on updated coordinates
+    world.style.transform = `
+        translateZ(600px) 
+        rotateX(${-pawn.rx}deg) 
+        rotateY(${-pawn.ry}deg) 
+        translate3d(${-pawn.x}px, ${-pawn.y}px, ${-pawn.z}px)
+    `;
 }
 
-TimerGame = setInterval(update, 10); 
+// Start the update loop
+setInterval(update, 10);
